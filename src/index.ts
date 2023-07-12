dd268ebb8cd2be04ef4ef2c9a900cb915a65d5ac
Bortolaci <joaopedrobortolaci2003@gmail.com>
2023-07-11 21:24:43 -0300

    feat: server express running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Gremio não vai sair campeão');
+//import express from 'express';
+const express = require('express');
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.get('/gremiovaisaircampeao', (request: any, response: any) => {
+    response.send('o gremio vai sair campeao');
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
+
+
+
+
+
+
+
